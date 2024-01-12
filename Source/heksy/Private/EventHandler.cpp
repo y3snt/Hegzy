47bab94fc8b0bce03d1ae12b298be81e02c0434f
Magdalena Dolesniak <magda.dolesniak@gmail.com>
2024-01-12 02:01:55 +0100

    Init custom Event System

--- Source/heksy/Private/EventHandler.cpp
+++ Source/heksy/Private/EventHandler.cpp
@@ -1,18 +1,9 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "EventHandler.h"
 
-/*
-template <typename... Args>
-EventHandler<Args...>::~EventHandler() { }
+template<class... Args>
+EventHandler<Args...>::EventHandler(const std::function<void (Args...)> &Func) : HandlerFunc(Func) {}
 
-template <typename... Args>
-EventHandler<Args...>::EventHandler(const std::function<void(Args...)>& Func) : HandlerFunc(Func) { }
-
-*/
-
-/*
-template <typename... Args>
+template<class... Args>
 void EventHandler<Args...>::operator()(Args... params) const  // EventHandler(...) operator overloading
 {
     if (HandlerFunc)
@@ -20,4 +11,3 @@ void EventHandler<Args...>::operator()(Args... params) const  // EventHandler(..
         HandlerFunc(params...);
     }
 }
-*/
--- Source/heksy/Public/HegzEvent.h
+++ Source/heksy/Public/HegzEvent.h
@@ -7,9 +7,19 @@
 /**
  * 
  */
+template<class... Args>
 class HEKSY_API HegzEvent
 {
-public:
-	HegzEvent();
-	~HegzEvent();
+public:	
+	void operator()(Args... params) const;
+	HegzEvent<Args...>& operator+=(const std::function<void(Args...)>& handler);  // ew. check if same handler is not added twice... but ==
+	HegzEvent<Args...>& operator+=(const EventHandler<Args...>& handler);
+	bool operator-=(const EventHandler<Args...>& handler);
+
+protected:
+	void Add(const std::function<void(Args...)>& handler);
+	void AddHandler(const EventHandler<Args...>& handler);
+	bool RemoveHandler(const EventHandler<Args...>& handler);
+
+    std::list< EventHandler<Args...> > Handlers;
 };
